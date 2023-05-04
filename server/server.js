235a658008d2d6cce75331205e3e8b204ad47c2f
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2023-05-04 20:31:34 +0530

    set up enviromental variables

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 5000;
+//getting enviroment variables
+const PORT = process.env.PORT;
 
+//express app
 const app = express();
 
+//middlewares
 app.use(cors());
 app.use(express.json());
 
+//routes
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
+//listen for requests
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
